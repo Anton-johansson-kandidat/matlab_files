d385818e7be226681df1e80260c04a563d954225
Anton-johansson-kandidat <erikantonjohansson@live.com>
2016-06-07 08:38:34 +0000

    Add files via upload

---
+++ CALC_FORCE.m
@@ -0,0 +1,200 @@
+function [F,timeone,timetwo,visk,p,vmedeltillplot,mfp,vlagring] = CALC_FORCE(y,n,length,lambdany)
+
+numberpeople=n; %Number of people
+length_corridor=length; %The length of the corridor
+fk = 1.6; %Force from the wall hade 0.5,1.3 funkade ganska bra
+he=10; %Defining lower wall
+ht=12.5; %Defining upper wall
+d=0.2; %Distance from the wall until they feel the force, quite large since you're always aware of the wall
+
+timeone = zeros(1,numberpeople); %Used to calculate the time it takes for them to pass through the corridor
+timetwo = zeros(1,numberpeople); %Used to calculate the time it takes for them to pass through the corridor
+
+F = zeros(4*numberpeople,1); %This is the vector that contains all information
+j=1; %This is used to create all the repulsive and attractive forces later on
+vloopvariable=1; %Used as a loop variable
+
+
+minussign=0; %Used to denote when we have passed half the population
+personalsphere=0.55; %This is their personalsphere,inside it they feel forces from other people
+
+aenemy=3.5; %This is used in the repelling force between individuals in different crowds
+afriend = 2; %The constant of repulsion for people in the same crowd
+b=personalsphere; %Constant used in repelling force
+alpha1 = 0.5; %For the x-driving force
+alpha2 = 8; %For the y-driving force
+v0=1.5; %Their desired speed hade 1.5
+lambda = lambdany; %The value of the perception anisotropy
+
+%For viscosity
+vlagring = zeros(1,numberpeople); %Used to store the velocities
+mfplagring = zeros(1,numberpeople); %Used to store the mean free path, capital lambda
+
+
+%This is the loop that creates the equations for all the persons
+for i=1:4:4*numberpeople %increase with 4 each step since there are for equation for each person
+    
+    if(i>=4*numberpeople/2) %Checks if we have passed half the population
+       minussign=1; 
+    end
+    
+    drivingforcex = ((-1)^(minussign)*v0-y(i+1))/alpha1; %The driving forcex
+    lowerwallrepulsion=0; %The lower wall repulsion
+    higherwallrepulsion=0; %The upper wall repulsion
+
+    if(abs(he-y(i+2))<d) %If they are within distance d from the wall they should feel the force
+    lowerwallrepulsion = -fk*(1-d/(abs(y(i+2)-he))); %Repulsion from the wall
+    end
+    if (abs(ht-y(i+2))<d)   %If they are within distance d from the wall they should feel the force
+    higherwallrepulsion = fk*(1-d/(abs(y(i+2)-ht))); %repulsion from thw wall
+    end
+    drivingforcey = alpha2*(-y(i+3)); %Calculates the driving forcey
+    pedestrepulsivex=0; %The repulsive force in x-direction
+    pedestrepulsivey=0; %The repulsive force in y-direction
+    
+    peopleinvicinity=0; %stores the number of people inside the radius of repulsion
+    peopleinvicinityny=0;
+    for k=1:4:4*numberpeople %This loop creates all the forces between the pedestrians hade 4*n-4
+        theta_calculated=0; %says that the angle has not been calcualted yet
+        if k ~= i %Check so the pedestrian wont exert forces on himself
+            
+            peddistance = sqrt((y(i+2)-y(k+2))^2+(y(i)-y(k))^2); %Distance between pedestrians
+            
+
+            
+            %The code below calculates the variables of those who feel
+            %each other across the boundary  
+            if y(i)<-7.1 && minussign==1 %Check so they are close to the boundary and they are traveling to the left
+                if (abs(y(i)-y(k))>14.2) %Check if the person is close to the other across the boundary
+                   peddistance = sqrt((y(i+2)-y(k+2))^2+((7.5-abs(y(i)))+(7.5-abs(y(k))))^2);%Calculate the distance between them
+                   theta = atan((y(i+2)-y(k+2))/((7.5-abs(y(i)))+(7.5-abs(y(k))))); %calculate the angle
+                   theta_calculated = 1; %The angle does not need to be calculated down below now
+                end
+            end
+            
+             if y(i)>7.1 && minussign==0 %Check so they are close to the boundary and traveling to the rigth
+                 if (abs(y(i)-y(k))>14.2) %Check so they are close to each other across the boundary
+                   peddistance = sqrt((y(i+2)-y(k+2))^2+((7.5-abs(y(i)))+(7.5-abs(y(k))))^2); %Calculate the distance between the pedestrians across the boundary
+                   theta = atan((y(i+2)-y(k+2))/((7.5-abs(y(i)))+(7.5-abs(y(k))))); %Calculate the angle
+                   theta_calculated = 1;%The angle does not need to be calculated below
+                end
+             end
+            %End of code for across the boundary
+            %Stores the mean free path
+            if (peddistance < personalsphere)
+                mfplagring(vloopvariable) = mfplagring(vloopvariable) + peddistance;
+            end
+            if peddistance < personalsphere
+                peopleinvicinityny=peopleinvicinityny+1; %Calculats the amount of people inside the personal radius
+            end
+            
+            
+            %The code below calculates the angle theta between pedestrians
+            %Kanske endast borde räkna ut dessa om de befinner sig i
+            %personalsphere?
+            if theta_calculated==0
+             if minussign ==0 && peddistance < personalsphere
+                if(y(i)<y(k))
+                   theta = atan((y(k+2)-y(i+2))/abs((y(i)-y(k)))); 
+                else
+                    theta =pi- atan((y(k+2)-y(i+2))/abs((y(i)-y(k))));
+                end
+                theta_calculated = 1;
+               
+             end
+             if minussign ==1 && peddistance < personalsphere
+                if(y(i)<y(k))
+                   theta = pi-atan((y(k+2)-y(i+2))/(abs(y(i)-y(k)))); 
+                else
+                    theta = atan((y(k+2)-y(i+2))/(abs(y(i)-y(k))));
+                end
+                theta_calculated = 1;
+             end
+            end
+            %End of code for theta
+            
+            
+            
+             %calculate the anisotropy
+            if theta_calculated ==1
+            anisotrop = lambda + (1-lambda)*(1-cos(pi-theta))*0.5;
+            end
+            %Sees how many are in close vicinity
+            if peddistance < personalsphere
+                peopleinvicinity=peopleinvicinity+1;
+            end
+            
+            %Calculates the repulsive forces
+            if (peddistance < personalsphere) && minussign==0 && k<2*numberpeople-1 %If they are in the left crowd and they feel a friend
+               pedestrepulsivex = pedestrepulsivex - anisotrop*afriend*exp((0.5-peddistance)/b)*cos(theta); %Calculates force in x-direction
+               pedestrepulsivey = pedestrepulsivey - anisotrop*afriend*exp((0.5-peddistance)/b)*sin(theta); %Calculates force in y-direction
+            end
+            if (peddistance < personalsphere) && minussign==0 && k>=2*numberpeople %If they are in the left crowd and they feel an enemy
+                pedestrepulsivex = pedestrepulsivex - anisotrop*aenemy*exp((0.5-peddistance)/b)*cos(theta); %Calculates force in x-direction
+               pedestrepulsivey = pedestrepulsivey - anisotrop*aenemy*exp((0.5-peddistance)/b)*sin(theta); %Calculates force in y-direction
+            end
+            if (peddistance < personalsphere) && minussign==1 && k<2*numberpeople-1 %If they are in the right crowd and they feel an enemy
+               pedestrepulsivex = pedestrepulsivex + anisotrop*aenemy*exp((0.5-peddistance)/b)*cos(theta); %Calculates force in x-direction
+               pedestrepulsivey = pedestrepulsivey - anisotrop*aenemy*exp((0.5-peddistance)/b)*sin(theta); %Calculates force in y-direction
+            end
+            if (peddistance < personalsphere) && minussign==1 && k>=2*numberpeople %If they are in the right crowd and they feel a friend
+               pedestrepulsivex = pedestrepulsivex + anisotrop*afriend*exp((0.5-peddistance)/b)*cos(theta); %Calculates force in x-direction
+               pedestrepulsivey = pedestrepulsivey - anisotrop*afriend*exp((0.5-peddistance)/b)*sin(theta); %Calculates force in y-direction
+            end
+            %End of repulsive forces
+        end
+            
+    end
+    
+    if peopleinvicinity ~=0
+    mfplagring(vloopvariable) = mfplagring(vloopvariable)/peopleinvicinityny;
+    end
+    j=j+4;
+    density_factor=exp(-0.4*(peopleinvicinity-1)); %Calculates the density factor
+    F(i:i+3,1) = [y(i+1);drivingforcex+density_factor*pedestrepulsivex;y(i+3);lowerwallrepulsion+higherwallrepulsion+drivingforcey+density_factor*pedestrepulsivey]; %The system of ODE:s
+    
+    %The code below
+    vlagring(vloopvariable) = sqrt(y(i+1)^2+y(i+3)^2);
+    
+    %Slut på kod for visk
+    
+    
+    
+    
+    %This belongs to the polarization
+    if minussign ==1 %Checks which crowd we are on
+    averageanglex(1,vloopvariable) = y(i+1); %This one is used in the polariation code
+    averageangley(1,vloopvariable) = y(i+3); %This one is used in the polarization code
+    vink(1,vloopvariable) = atan(y(i+3)/y(i+1)); %Calculate the angle
+    end
+    %End of polariation code
+    vloopvariable =vloopvariable +1;
+    
+end
+
+if sum(mfplagring~=0) == 0 %calculates the mean free path
+    mfp = personalsphere;
+else
+mfp = sum(mfplagring)/sum(mfplagring~=0);
+end
+visk = 1/mean(vlagring)*1/mfp; %calculates the viscosity
+
+vmedeltillplot = mean(vlagring); %The mean velocity
+
+vxxx = sum(averageanglex)/(numberpeople/2);
+vyyy = sum(averageangley)/(numberpeople/2);
+vvektor = sqrt(vxxx^2+vyyy^2); %The veclosity vector is stored here
+
+    
+%detta hör till polariering
+thetamedel = (sum(averageangley)/(numberpeople/2))/(sum(averageanglex)/(numberpeople/2)); %Calculate the average angle of motion
+d = zeros(1,numberpeople/2); %The d used in the polarization
+for r=1:numberpeople/2 %Loops over half the crowd
+    d(1,r) = vink(1,r)-thetamedel;
+    d(1,r) = rem(d(1,r),2*pi);
+    
+    d(1,r) = abs(d(1,r));
+end
+p = sum(d)/(numberpeople/2); %The polarization
+    
+end
---
+++ SOLVE_SYSTEM.m
@@ -0,0 +1,200 @@
+
+%Different relusions for friend and enemy, increase relusive as well
+%It doesn't work that well for c<0.2
+global h L time numberofpeople xspawn upperwall lowerwall lambda
+
+upperwall = 12.5; %Defining upperwall
+lowerwall = 10; %Defining lowerwall
+xspawn = 10; %The distance they will spawn from the corridor
+L=20; %Length of corridor
+time=60; %Time of simulation
+h=0.01; %Timestep
+t=[0:h:time]; %Defining the timeinterval
+numberofpeople=100; %number of people
+lambda =1; %The value of the perception anisotropy
+SIZE_OF_t = size(t); %Stores the amount of timesteps
+storage_of_solution = zeros(4*numberofpeople,SIZE_OF_t(2)); %Stores all the information about the solution
+v=zeros(1,numberofpeople); %Used to store velocities
+xzero = zeros(1,4*numberofpeople); %Used to store solution as well
+
+
+%Starting positions for left crowd
+deltay=11.1; %Starting height
+deltax =-L-xspawn-rand; %Starting x-position
+
+for d=1:4:4*numberofpeople/2
+        xzero(d) = deltax; %Sets the x-starting position
+        xzero(d+2) = deltay; %Sets the y-starting position
+        
+        deltax = deltax + 0.6; %The step in x-direction
+        deltay=deltay+rand; %The y-psotions is randomized
+        if deltax >-14 %When to stop the x-position spawn
+           deltax=-L-xspawn+rand; %Move the starting position back
+        end
+        if deltay >12.3 %When to stop the y-psotion spawn
+           deltay=10+rand; %Move the starting position back
+        end
+    
+       
+end
+%End of starting position for left crowd
+
+%Starting positions for right crowd
+deltay=11; %starting height for the y-position
+deltax =L+xspawn+rand; %starting x-position
+for d=4*numberofpeople/2+1:4:4*numberofpeople-2
+        xzero(d) = deltax; %Sets the x-starting position
+        xzero(d+2) = deltay; %Sets the y-starting position
+        deltax = deltax - 0.6; %Takes one step forward in x
+        deltay=deltay+rand; %Takes a step in y
+        if deltax <14 %When to stop advancing in x
+           deltax=L+xspawn+rand; %Resets the x-value
+        end
+        if deltay >12.3 %When to stop in y
+           deltay=10+rand; %Reset the y-value
+        end
+end
+%End of starting position for right crowd
+
+timeett=zeros(10,numberofpeople);%Used to store the time when they arrive in the corridor
+timetva=zeros(10,numberofpeople);%Used to store the time when they leave in the corridor
+k=1; %Loop variable
+j=1; %Loop variable
+tidvariab1 =ones(1,numberofpeople); %Stores the time when they enter the periodic corridor
+tidvariab2 =ones(1,numberofpeople); %Stores the time when they leave the peridoci corridor
+visclagring = zeros(1,SIZE_OF_t(2)); %Stores the lane viscosity
+plagring = zeros(1,SIZE_OF_t(2)); %Stores the polarization values
+storage_of_solution = zeros(4*numberofpeople,SIZE_OF_t(2)); %Stores the solution to the Runge-Kutta method
+
+
+lambdalagring = zeros(1,SIZE_OF_t(2));
+spaceviscv = zeros(SIZE_OF_t(2),numberofpeople);
+
+
+for i=0:h:time
+    j=1;
+    storage_of_solution(:,k) = xzero'; %Stores the solution vector each round so that it can be plotted later on
+    %Runge-Kutta method
+    [k1,time1,time2,visc,p,spaceviscvek] = CALC_FORCE(xzero,numberofpeople,L,lambda);
+    [k2,time1,time2,visc,p,spaceviscvek] = CALC_FORCE(xzero+h*k1'/2,numberofpeople,L,lambda);
+    [k3,time1,time2,visc,p,spaceviscvek] = CALC_FORCE(xzero+h*k2'/2,numberofpeople,L,lambda);
+    [k4,time1,time2,visc,p,spaceviscvek] = CALC_FORCE(xzero+h*k3',numberofpeople,L,lambda);%Calls on the system of ODE:S
+    xzero = xzero+h*(k1'+2*k2'+2*k3'+k4')/6'; %Runge-kutta method method
+    plagring(k)=p; %Stores the polarization
+    visclagring(k) = visc; %Stores part of the lane viscosity
+
+    spaceviscv(k,:) = spaceviscvek; %Stores the space-dependant viscosity index values
+    %The code below is used for the periodic boundary conditions
+   for d=1:4:4*numberofpeople/2
+         if xzero(1,d)>6.4 %Periodic corridor BC
+             xzero(1,d)=-6.4; %Resets the x-position to the other side of the corridor
+             timeett(tidvariab1(1,j),j)=k; %Stores the time they leave the periodic corridor
+           tidvariab1(1,j) = tidvariab1(1,j)+1; %Says that we have calculated the value of the time
+         end
+         j=j+1; %Increases a loop-variable
+   end
+   for d=2*numberofpeople+1:4:4*numberofpeople
+         if xzero(1,d)<-6.4 %Periodic corridor BC
+            xzero(1,d)=6.4; %Resets the x-position value
+            timeett(tidvariab1(1,j),j)=k; %Stores the time they leave the corridor
+            tidvariab1(1,j) = tidvariab1(1,j)+1; % Says that we have calcualted the value of the time
+         end
+         j=j+1; %Increases a loop-variable
+   end 
+   %End of boundary conditions code
+    k=k+1; %Increases a loop-variable
+    
+end
+storage_of_solution = storage_of_solution'; %Transposes the storage vector
+
+
+
+%plot for spaceviscosity
+%start of code
+[X,Y] = meshgrid(-7:0.1:7,9.5:0.1:13); %Creates the grid which is used to plot the space-dependant viscosity index
+
+Space_dependent_viscosity=0; %Sets the function value to 0 first for the space-dependant viscosity index
+
+
+
+%The code Below plots the space-dependent viscosity index and the
+%correspond sparital configuartion of pedestrians
+k=1;
+time_for_plot_SPV = 4000;
+for SPV_loop_vairable=1:numberofpeople
+Space_dependent_viscosity = Space_dependent_viscosity +  1/(spaceviscv(time_for_plot_SPV,SPV_loop_vairable)+0.2)*exp(-((X-storage_of_solution(time_for_plot_SPV,k)).^2+(Y-storage_of_solution(time_for_plot_SPV,k+2)).^2));    
+k=k+4;
+end
+figure;
+pcolor(X,Y,Space_dependent_viscosity); shading interp;
+%The code below plots the spatial configuaration
+figure;
+for b=1:4:4*numberofpeople/2
+plot(storage_of_solution(time_for_plot_SPV,b),storage_of_solution(time_for_plot_SPV,b+2),'ro')
+axis([-6 6 lowerwall-0.5 upperwall+0.5])
+hold on
+end
+for b=4*numberofpeople/2+1:4:4*numberofpeople
+plot(storage_of_solution(time_for_plot_SPV,b),storage_of_solution(time_for_plot_SPV,b+2),'bx')
+axis([-6 6 lowerwall-0.5 upperwall+0.5])
+hold on
+end
+plot([-L/2-20,L/2+20],[upperwall,upperwall],'k','LineWidth',4)
+    hold on
+    plot([-L/2-20,L/2+20],[lowerwall,lowerwall],'k','LineWidth',4) 
+    hold on
+ylabel('y (m)')
+xlabel('x (m)')
+%End of code for space-dependent viscosity index
+
+%The code below is used for the viscosity and polarization
+
+%Used to calculate the mean polarization
+viscplot = visclagring(2:end);
+tplot = t(200:end-200);
+ptidsmedel = zeros(1,SIZE_OF_t(2));
+pepsilon = zeros(1,SIZE_OF_t(2)-399);
+
+
+
+plagringjamvikt = plagring(350);
+for u=1:400
+plagring(u) = plagringjamvikt;
+end
+
+for j=2:SIZE_OF_t(2)  
+ptidsmedel(j) = sum(plagring((2:j)))/(sum(plagring((2:j))>0));
+end
+k=1;
+for i=400:SIZE_OF_t(2)-199
+    for o=i-199:i+199
+pepsilon(k) = pepsilon(k)+plagring(o);
+    end
+    pepsilon(k)=pepsilon(k)/400;
+k=k+1;
+end
+%End of code for polarization mean
+
+
+vmedel = sum(visclagring((SIZE_OF_t(2)-1)/2:end))/(sum(visclagring((SIZE_OF_t(2)-1)/2:end)>0));
+vmedelny = visclagring(200:end-200);
+%To plot mean polarization and the lane viscosity index. Remove the comment
+%below
+%{
+plot(tplot,pepsilon)
+ylabel('P(t)')
+xlabel('t (s)')
+axis([0 60 0 0.1])
+lambda = lambda-0.25;
+%}
+
+%{
+viskositeten = vmedelny.*pepsilon;
+figure;
+plot(tplot,viskositeten)
+ylabel('\mu (t)')
+xlabel('t (s)')
+axis([0 70 0 0.1])
+%}
+
+
